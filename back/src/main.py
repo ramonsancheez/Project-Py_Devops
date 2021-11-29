fcbb691bd7a30f25f40fd937434ec35ba533805b
Ramon <rsanchezabadia@cifpfbmoll.eu>
2021-11-29 20:01:46 +0100

    Reorganitation update

--- back/src/main.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-from pprint import pprint
-
-cluster = MongoClient("mongodb+srv://m001-student:mongo123@cluster0.qmgmx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-db = cluster.proyecto
-documentos = db.menus.find()
-# Pprint se utiliza para visualizar los datos de forma esquemática
-for i in documentos:    
-    pprint(i)
