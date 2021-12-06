6aadb3363c01f6c07d80f5e356b3f0fc5e220cbd
Ramon <rsanchezabadia@cifpfbmoll.eu>
2021-12-06 19:27:15 +0100

    refactor:

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,14 +1,15 @@
-# from accesoDatos.conexionBasedatos import connectionBBDD
-# from logica.convertirMarkdown.convertirMarkdown import creadorMarkdown
-# from os import system
+from accesoDatos.conexionBasedatos import connectionBBDD
+from logica.convertirMarkdown.convertirMarkdown import creadorMarkdown
 from logica.conectarHugo.conectarHugo import conectarHugo 
-# system('cls')
+from os import system
+system('cls')
 
 def principal():
     
-    # baseDatos = connectionBBDD()
-    # creadorMarkdown(baseDatos, "Comida Chatarra")
-    # creadorMarkdown(baseDatos, "Comida Vegana Intergaláctica")
-    # creadorMarkdown(baseDatos, "Comida Baby Yoda")
+    baseDatos = connectionBBDD()
+    creadorMarkdown(baseDatos, "Comida Chatarra")
+    creadorMarkdown(baseDatos, "Comida Vegana Intergaláctica")
+    creadorMarkdown(baseDatos, "Comida Baby Yoda")
     conectarHugo()
+
 principal()
