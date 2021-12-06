ae72b56feeeb48f0b7faab2c158cc45a66bd5973
Ramon <rsanchezabadia@cifpfbmoll.eu>
2021-12-06 19:25:46 +0100

    refactor: :zap: TryExcept y Factorizar codigo

--- backend/src/logica/conectarHugo/conectarHugo.py
+++ backend/src/logica/conectarHugo/conectarHugo.py
@@ -1,33 +1,20 @@
-#El módulo OS nos da funcionalidades específicas del sistema operativo, como:
-# crear una carpeta, listar contenidos de una carpeta, 
-# conocer acerca de un proceso, finalizar un proceso,etc
 import os 
-
-#El módulo SH nos permite hacer operaciones de alto nivel en archivos y 
-# colecciones de archivos. Ej: copiar, mover, eliminar...
 import shutil
 
-# Para saber el contenido del directorio del que vamos a copiar
-contenido = "Este es el contenido que hay en la carpeta: \n" +  str(os.listdir(f'{os.getcwd()}/archivosMarkdown/'))
-print(contenido)
-print(os.getcwd())
-
 def conectarHugo():
-    
-#Establecemos la ruta de origen y destino en variables para manejar mejor el código
-    rutaOrigen = f'{os.getcwd()}/archivosMarkdown/'
-    rutaDestino =f'{os.getcwd()}/frontend/webProyecto/content/'
+    try:
+        rutaOrigen = f'{os.getcwd()}/archivosMarkdown/'
+        archivosLista = os.listdir(rutaOrigen)
+        assert archivosLista
 
-#Creamos una lista de los archivos que con contiene la carpeta origen 
-# en forma de variable
-    archivos = os.listdir(rutaOrigen)
-
-    for archivo in archivos:
-        #SHUTIL.COPY copia lo que está en primer lugar, ya sea un archivo específico 
-        #o ruta en el lugar que indicamos después de la ",". 
-        # Y OS.PATH.JOIN combina uno o más nombres de ruta en una única ruta.
-        shutil.copy(os.path.join(rutaOrigen, archivo), rutaDestino)
-    
-    return conectarHugo
-
-
+    except:
+        print("La carpeta archivosMarkdown está vacía, por lo que no se le puede transferir ningún archivo a HUGO")
+        exit()
+    else:
+        contenido = "Esta es una lista del contenido que hay en la carpeta archivosMarkdown: \n" +  str(archivosLista)
+        rutaOrigen = f'{os.getcwd()}/archivosMarkdown/'
+        rutaDestino =f'{os.getcwd()}/frontend/webProyecto/content/'
+        for archivo in archivosLista:
+            shutil.copy(os.path.join(rutaOrigen, archivo), rutaDestino)
+        print(contenido)
+        return conectarHugo
