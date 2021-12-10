96ffae9793ba47b768c9b71f10ed3eb08e6e9145
LeslieR0SS <laranibarpozo@cifpfbmoll.eu>
2021-12-10 20:49:41 +0100

    feat: :sparkles: implementar la funcion de lanzar hugo

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,7 @@
 from accesoDatos.conexionBasedatos import connectionBBDD
 from logica.convertirMarkdown.convertirMarkdown import creadorMarkdown
 from logica.conectarHugo.conectarHugo import conectarHugo 
+from logica.abrirHugo.abrirHugo import abrirHugo
 from os import system
 system('cls')
 
@@ -9,7 +10,10 @@ def principal():
     baseDatos = connectionBBDD()
     creadorMarkdown(baseDatos, "ComidaChatarra")
     creadorMarkdown(baseDatos, "ComidaVeganaIntergalactica")
-    creadorMarkdown(baseDatos, "ComidaBabyYoda")
+    creadorMarkdown(baseDatos, "ComidaBabyYoda")    
     conectarHugo()
+    abrirHugo()
+
+    
 
 principal()
