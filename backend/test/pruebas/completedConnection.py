3cbfe48218d0dd3c8752b5a5ae4af7fbddaa4220
Ramon <rsanchezabadia@cifpfbmoll.eu>
2021-12-11 12:43:09 +0100

    test: :white_check_mark: Comprueba que en esa url existe la collection que buscamos, sino va al except y devuelve false

---
+++ backend/test/pruebas/completedConnection.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+import certifi
+
+def connectionBBDD(url):
+    try:
+        cluster = MongoClient(url, tlsCAFile=certifi.where())
+        baseDatos = cluster.proyecto
+        "menus_with_schema" in baseDatos.list_collection_names()
+    except:
+        print("No pudo realizarse la conexion con la base de datos")
+        return False
+    else:
+        print("La conexión con la base de datos se realizó correctamente \n")
+        return True
--- backend/test/test_isInsideDB.py
+++ backend/test/test_isInsideDB.py
@@ -1,9 +1,14 @@
-from backend.src.accesoDatos.conexionBasedatos import connectionBBDD
+from backend.test.pruebas.completedConnection import connectionBBDD
 import pytest
 
 @pytest.mark.connectionBBDD
-def test_BBDD_no_vacia():
-    baseDatos = connectionBBDD()
-    assert baseDatos != None
+def test_BBDD_url_correcta():
+    url = "mongodb+srv://m001-student:mongo123@cluster0.qmgmx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    baseDatos = connectionBBDD(url)
+    assert baseDatos == True
     
-    
+@pytest.mark.connectionBBDD
+def test_BBDD_url_incorrecta():
+    url = "urlErronea"
+    baseDatos = connectionBBDD(url)
+    assert baseDatos == False   
