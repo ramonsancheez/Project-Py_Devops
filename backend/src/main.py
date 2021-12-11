e7d7e2740189fbd1615363de23d01d533f1daa4a
Ramon <rsanchezabadia@cifpfbmoll.eu>
2021-12-11 19:30:04 +0100

    refactor: :zap: Mas single responsability

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,17 +3,18 @@ from logica.convertirMarkdown.convertirMarkdown import creadorMarkdown
 from logica.conectarHugo.conectarHugo import conectarHugo 
 from logica.abrirHugo.abrirHugo import abrirHugo
 from os import system
-system('cls')
+system("cls")
+
 
 def principal():
     
-    baseDatos = connectionBBDD()
+    url = "mongodb+srv://m001-student:mongo123@cluster0.qmgmx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+    baseDatos = connectionBBDD(url)
     creadorMarkdown(baseDatos, "ComidaChatarra")
     creadorMarkdown(baseDatos, "ComidaVeganaIntergalactica")
     creadorMarkdown(baseDatos, "ComidaBabyYoda")    
     conectarHugo()
     abrirHugo()
-
-    
 
 principal()
