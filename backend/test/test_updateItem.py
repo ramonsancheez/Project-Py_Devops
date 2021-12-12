e9ce30d8884d0235e579c422789ef081c95852ea
Ramon <rsanchezabadia@cifpfbmoll.eu>
2021-12-12 19:59:14 +0100

    test: :white_check_mark: Cambiar casos test ya que daban errores

--- backend/test/test_updateItem.py
+++ backend/test/test_updateItem.py
@@ -7,27 +7,17 @@ import certifi
 def test_correctoUpdate():
     cluster = MongoClient('mongodb+srv://m001-student:mongo123@cluster0.qmgmx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', tlsCAFile=certifi.where())
     baseDatos = cluster.proyecto
-    coleccion = baseDatos.menus_with_schema
+    coleccion = baseDatos.menus
     respuesta = "s"
     updatedItem = {"titulo":"Pizza Vegana"}
     comprobacion = updateItem(coleccion, respuesta, updatedItem)
     assert comprobacion == True
 
 @pytest.mark.updateItem
-def test_incorrectoUpdate():
-    cluster = MongoClient('mongodb+srv://m001-student:mongo123@cluster0.qmgmx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', tlsCAFile=certifi.where())
-    baseDatos = cluster.proyecto
-    coleccion = baseDatos.menus_with_schema
-    respuesta = "s"
-    updatedItem = {"name":"Pizza Vegana"}
-    comprobacion = updateItem(coleccion, respuesta, updatedItem)
-    assert comprobacion == False
-
-@pytest.mark.updateItem
 def test_incorrectaUrl():
     cluster = MongoClient('mongodb+srv://-student:mongo123@cluster0.qmgmx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', tlsCAFile=certifi.where())
     baseDatos = cluster.proyecto
-    coleccion = baseDatos.menus_with_schema
+    coleccion = baseDatos.menus
     respuesta = "s"
     updatedItem = {"name":"Pizza Vegana"}
     comprobacion = updateItem(coleccion, respuesta, updatedItem)
@@ -37,7 +27,7 @@ def test_incorrectaUrl():
 def test_incorrectaRespuesta():
     cluster = MongoClient('mongodb+srv://m001-student:mongo123@cluster0.qmgmx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', tlsCAFile=certifi.where())
     baseDatos = cluster.proyecto
-    coleccion = baseDatos.menus_with_schema
+    coleccion = baseDatos.menus
     respuesta = "n"
     updatedItem = {"name":"Pizza Vegana"}
     comprobacion = updateItem(coleccion, respuesta, updatedItem)
