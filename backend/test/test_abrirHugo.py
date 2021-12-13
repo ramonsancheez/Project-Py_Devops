bfc8f6a05f0ccdb0b72a6efbe22a3b54e7a22047
Ramón Sánchez <91556453+ramonsancheez@users.noreply.github.com>
2021-12-13 15:41:50 +0100

    delete caso test hugo

--- backend/test/test_abrirHugo.py
+++
@@ -1,8 +0,0 @@
-from backend.test.pruebas.abrirHugo import abrirHugo
-import pytest
-
-@pytest.mark.abrirHugo
-def test_URLcorrect():
-    hugoUrl = "http://localhost:1313/"
-    URLcorrect = abrirHugo(hugoUrl)
-    assert URLcorrect == True
